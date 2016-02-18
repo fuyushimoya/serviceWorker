8bf0552fb529d146b0a4cb5de770f42f13f2dd14
chinyu_lin <chinyu_lin@trend.com.tw>
2016-02-18 16:28:22 +0800

    Add button and handlers.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log("Test");
+// Functions
+function registerService() {
+    console.log("registerService Not implement yet");
+}
+function unregisterService() {
+    console.log("unregisterService Not implement yet");
+}
+function runWebWorker() {
+    console.log("runWebWorker Not implement yet");
+}
+
+(function domReadyHandler() {
+    console.log('Dom ready, start to do something...');
+
+    var registerServiceBtn = document.getElementById('registerService');
+    var unRegisterServiceBtn = document.getElementById('unRegisterService');
+    var runWebWorkerBtn = document.getElementById('worker');
+
+    registerServiceBtn.addEventListener('click', registerService);
+    unRegisterServiceBtn.addEventListener('click', unregisterService);
+    runWebWorkerBtn.addEventListener('click', runWebWorker);
+})();
